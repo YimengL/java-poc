f99bc378f84cf58019cf3fa2fa36b100fcdc304f
Yimeng Li <yimengcsu@gmail.com>
2021-05-15 20:24:36 +0200

    Update TimeZone

--- udemy-spring-hibernate/hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
+++ udemy-spring-hibernate/hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
@@ -7,7 +7,7 @@ public class TestJdbc {
 	public static void main(String[] args) {
 		
 		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL="
-				+ "false&serverTimezon=UTC";
+				+ "false&serverTimezone=Europe/Berlin";
 		String user = "hbstudent";
 		String pass = "hbstudent";
 		try {
